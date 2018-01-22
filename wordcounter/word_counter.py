47d43572a8d84591b3d8129fec9a33dc11a7af42
paracelsus-von <phara0h1@hotmail.com>
2018-01-22 15:07:05 +0000

    Adding csv file

--- wordcounter/word_counter.py
+++ wordcounter/word_counter.py
@@ -2,6 +2,7 @@ from collections import Counter
 import glob
 import os
 import io
+import csv
 
 directory = raw_input("Directory of files to analyse: ")
 if directory == "":
@@ -27,15 +28,26 @@ for filename in glob.glob("*.txt"):
 
     # Cleaning source text
     processed_text = map(str.lower, file_open.read().split())
-    for char in [".", ",", "?", "!", "(", ")", "l'", "d'"]:
+    for char in [".", ",", "?", "!", "(", ")", "*", "+", "l'", "d'"]:
         processed_text = [x.strip(char) for x in processed_text]
 
     # Adding text frim each file to master text list
     text += processed_text
 
 wordcount = Counter(text)
+sorted_wordcount = sorted(wordcount.items(), key=lambda pair: pair[1], reverse=True)
+sorted_wordcount_clean = []
 
-for item in sorted(wordcount.items(), key=lambda pair: pair[1], reverse=True):
-    if item[0] not in bad_words and item[1] > 2 and "/" not in item[0] and "4" not in item[0] and "2" not in item[0]:
+for item in sorted_wordcount:
+    if item[0] not in bad_words and item[1] > 2 and "/" not in item[0] and "4" not in item[0] and "3" not in item[0] and "2" not in item[0] and len(item[0])>0:
+        sorted_wordcount_clean.append(item)
         print("{}\t{}".format(*item))
 
+answer = raw_input("\n\n-----------------\n\nDo you want to save this as a .csv file?")
+
+if answer.lower() != "no":
+    with open('word_count.csv','wb') as out:
+        csv_out=csv.writer(out)
+        csv_out.writerow(['word','frequency'])
+        for row in sorted_wordcount_clean:
+            csv_out.writerow(row)
